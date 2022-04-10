eec464f838080949e5e7ae3b07aeebc0cc9b012e
Feiyu Zheng <feiyuzheng98@gmail.com>
2022-04-10 02:08:35 -0400

    Update the app of the project

--- boundless_analytics_test_project/settings/base.py
+++ boundless_analytics_test_project/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -36,13 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testproject'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
---
+++ testproject/views.py
@@ -0,0 +1,104 @@
+import traceback
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
+from django.db import connections
+import pandas as pd
+import plotly.express as px
+import sqlparse
+
+
+def index(request) -> HttpResponse:
+    return render(request, 'index.html')
+
+
+def querySubmit(request) -> JsonResponse:
+    cursor = None
+    try:
+        cursor = connections['default'].cursor()
+    except Exception as e:
+        responseStatus = 1
+        traceback.print_exc()
+        return JsonResponse(
+            {
+                'responseStatus': responseStatus,
+                'errorDetails': str(e)
+            }
+        )
+
+    zeroQuery = "select * from moody2022_new"
+    sliceQuery = str(request.POST.get('query'))
+
+    try:
+        sliceQuery = sqlparse.format(sliceQuery, reindent=True, keyword_case='upper')
+        sliceQuery = sqlparse.split(sliceQuery)[0]
+
+        cursor.execute(zeroQuery)
+        rows = cursor.fetchall()
+
+        df_zero = pd.DataFrame([ij for ij in i] for i in rows)
+        df_zero.rename(
+            columns={
+                0: 'SCORE',
+                1: 'GRADE',
+                2: 'DOZES_OFF',
+                3: 'TEXTING_IN_CLASS',
+                4: 'PARTICIPATION'
+            },
+            inplace=True
+        )
+        df_zero['TYPE'] = "Zero"
+
+        cursor.execute(sliceQuery)
+        rows = cursor.fetchall()
+        df_slice = pd.DataFrame([ij for ij in i] for i in rows)
+        df_slice.rename(
+            columns={
+                0: 'SCORE',
+                1: 'GRADE',
+                2: 'DOZES_OFF',
+                3: 'TEXTING_IN_CLASS',
+                4: 'PARTICIPATION'
+            },
+            inplace=True
+        )
+        df_slice["TYPE"] = "Slice"
+
+        df = pd.concat([df_zero, df_slice])
+
+        plot = px.box(
+            df,
+            x='GRADE',
+            y='SCORE',
+            color='TYPE'
+        )
+        plot.update_layout(
+            {
+                "title": {
+                    "text": "Distribution of Scores by Grade",
+                    "x": 0.5
+                },
+                "xaxis": {
+                    "title": "Letter Grade"
+                },
+                "yaxis": {
+                    "title": "Score (out of 100)"
+                }
+            }
+        )
+        plot.update_xaxes(categoryorder="category ascending")
+        plot.write_html("media/result.html")
+
+    except Exception as e:
+        responseStatus = 1
+        traceback.print_exc()
+        return JsonResponse({
+            'responseStatus': responseStatus,
+            'errorDetails': str(e)
+        })
+
+    return JsonResponse(
+        {
+            'query': sliceQuery
+        }
+    )
